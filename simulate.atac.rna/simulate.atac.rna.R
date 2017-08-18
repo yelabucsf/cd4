cba70a8fea0b0d2d6f8d7ab29df00c632a57da40
yimmieg <yimmieg@gmail.com>
2017-08-17 21:09:13 -0700

    more realistic parameters.

--- simulate.atac.rna/simulate.atac.rna.R
+++ simulate.atac.rna/simulate.atac.rna.R
@@ -2,8 +2,8 @@ library(qvalue);
 
 ## simulate atac and rna
 
-atac.beta <- 0.4;
-rna.beta <- 0.4;
+atac.beta <- 0.6;
+rna.beta <- 0.6;
 
 n <- 100;
 
@@ -16,13 +16,17 @@ gen <- hap1+hap2;
 
 atac.ps <- NULL;
 rna.ps <- NULL;
+atac.r2 <- NULL;
+rna.r2 <- NULL;
 ps <- NULL;
 
 for (i in 1:1000) {
-  atac <- atac.beta*gen+rnorm(n,0.5);
-  rna <- rna.beta*gen+rnorm(n,0.5);
+  atac <- atac.beta*gen+rnorm(n,0.05);
+  rna <- rna.beta*gen+rnorm(n,0.05);
   atac.ps <- c(atac.ps, summary(lm(atac~gen))$coefficients[2,4]);
+  atac.r2 <- c(atac.r2, summary(lm(atac~gen))$r.squared);
   rna.ps <- c(rna.ps, summary(lm(rna~gen))$coefficients[2,4]);
+  rna.r2 <- c(rna.r2, summary(lm(rna~gen))$r.squared);
   ps <- c(ps, summary(lm(rna~atac))$coefficients[2,4]);
 }
 
